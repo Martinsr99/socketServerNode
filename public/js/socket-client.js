59964fc3239aaf13b77f4a645a9e886d84033441
Martin Siles <silesreche.martin@gmail.com>
2022-10-16 22:42:41 +0200

    Allow client to send messages to backend

---
+++ public/js/socket-client.js
@@ -0,0 +1,36 @@
+
+
+//refencias HTML
+const lblOnline = document.querySelector('#lblOnline')
+const lblOffline = document.querySelector('#lblOffline')
+const txtMensaje = document.querySelector('#txtMensaje')
+const btnEnviar = document.querySelector('#btnEnviar')
+
+const socket = io();
+
+socket.on('connect',() => {
+    console.log('Conectado');
+
+    lblOffline.style.display = 'none';
+    lblOnline.style.display = '';
+
+})
+
+
+socket.on('disconnect',() => {
+    console.log('Desconectado')
+
+    lblOnline.style.display = 'none';
+    lblOffline.style.display = '';
+})
+
+btnEnviar.addEventListener('click',() => {
+
+    const mensaje = txtMensaje.value
+    const payload = {
+        mensaje,
+        id: '123ABC',
+        fecha: new Date().getTime()
+    }
+    socket.emit('enviar-mensaje', payload);
+})
